37c1725ec73dd8c6f5237964199dd3e7ea0dce8d
ohgus <123801385+ohgus@users.noreply.github.com>
2024-12-12 17:29:09 +0900

    feat: controller 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,18 +1,102 @@
-const InputView = require('../view/InputView.js');
-const OutputView = require('../view/OutputView.js');
+import InputView from '../view/InputView.js';
+import OutputView from '../view/OutputView.js';
+import BridgeGame from '../model/BridgeGame.js';
+import Player from '../model/Player.js';
+
+import validateSize from '../util/validateSize.js';
+import validateMove from '../util/validateMove.js';
 
 class Controller {
-  init() {
-    OutputView.printGreet();
-    this.#readSize();
+  #bridgeGame;
+  #player;
+
+  constructor() {
+    this.#player = new Player();
   }
 
-  #readSize() {
-    InputView.readBridgeSize((input) => {
-      const size = input;
-      // 다음 로직 구현
-    });
+  async init() {
+    OutputView.printGreet();
+
+    const size = await this.#readSize();
+    this.#bridgeGame = new BridgeGame(size);
+    await this.#moveProcess(size);
+    const finalResult = this.#bridgeGame.getResult();
+    OutputView.printResult(this.#player.getPosition(), finalResult);
+  }
+
+  async #moveProcess(size) {
+    let tryCount = size;
+
+    while (tryCount) {
+      const moveResult = await this.#move();
+      const canMoveAgain = this.#checkGameOver(moveResult);
+
+      if (!canMoveAgain) {
+        const command = await this.#readCommand();
+        if (command === 'Q') {
+          tryCount = 0;
+          break;
+        } else {
+          this.#bridgeGame.retry();
+          this.#player.retry();
+          return await this.#moveProcess(size);
+        }
+      }
+
+      tryCount -= 1;
+    }
+  }
+
+  #checkGameOver(moveResult) {
+    if (moveResult === 'X') {
+      return false;
+    }
+    return true;
+  }
+
+  async #readSize() {
+    try {
+      const size = await InputView.readBridgeSize();
+      validateSize(size);
+      return size;
+    } catch (e) {
+      OutputView.printError(e.message);
+      return await this.#readSize();
+    }
+  }
+
+  async #readMove() {
+    try {
+      const moveTo = await InputView.readMoving();
+      validateMove(moveTo);
+
+      return moveTo;
+    } catch (e) {
+      OutputView.printError(e.message);
+      return await this.#readMove();
+    }
+  }
+
+  async #move() {
+    const moveTo = await this.#readMove();
+    const moveResult = this.#bridgeGame.move(moveTo);
+
+    this.#player.updatePosition(moveResult, moveTo);
+    OutputView.printMap(this.#player.getPosition());
+
+    return moveResult;
+  }
+
+  async #readCommand() {
+    try {
+      const command = await InputView.readGameCommand();
+
+      return command;
+    } catch (e) {
+      OutputView.printError(e.message);
+      return await this.#readCommand();
+    }
   }
 }
 
-module.exports = Controller;
+export default Controller;
